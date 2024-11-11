b6d1e415bb046860f4f9b3155fba5b12eec8a9ae
Dustin <135719141+DustinTurska@users.noreply.github.com>
2024-11-11 11:11:59 -0600

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThirdwebProvider } from "thirdweb/react";
+import { cn } from "@/lib/utils"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,8 +18,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
+    <html lang="en" className="dark">
+    <body className={cn(
+      "min-h-screen bg-background font-sans antialiased",
+    )}>
         <ThirdwebProvider>{children}</ThirdwebProvider>
       </body>
     </html>
